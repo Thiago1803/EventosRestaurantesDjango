6f28772cfc5411cc5bb45ef36cc6b0e8ddf72585
ThiagoHenrique <thiago-maglhaes@hotmail.com>
2024-11-12 02:27:16 -0300

    commit6-realizando buscas por restaurantes na tela inicial

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Restaurants
 
-# Register your models here.
+
+@admin.register(Restaurants) # aqui faz aparecer o menuzinho de restaurantes la no ambiete admin
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone', 'email')
+    list_display_links = ('name',)
+    search_fields = ('name',)
--- setup/views.py
+++ setup/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import logout as django_logout
+from restaurants.models import Restaurants
 
 
 def logout_view(request):
@@ -9,4 +10,11 @@ def logout_view(request):
 
 
 def home(request):
-    return render(request, 'home.html')
+    search = request.GET.get('search', None)  # Pegando o que foi buscado e armazenando em "search", se nada foi buscado entao o valor é "none"
+
+    if search: # Procuro no BD restaurantes que POSSUEM EM SEU NOME o que foi buscado
+        restaurantes = Restaurants.objects.filter(name__icontains=search) 
+    else: # Se nada foi buscado, pego tudo
+        restaurantes = Restaurants.objects.all()
+
+    return render(request, 'home.html', {'restaurantes': restaurantes, 'search': search})
