c15f41ae70ad31f1e23c9c67ef92b0f269008ae8
ThiagoHenrique <thiago-maglhaes@hotmail.com>
2024-11-28 21:20:31 -0300

    commit9-arrumando filtro pelo clique no ícone

--- setup/views.py
+++ setup/views.py
@@ -13,26 +13,28 @@ def home(request):
     city = request.GET.get('city', None)  # Pegando a cidade que foi buscada, se nada foi buscado entao o valor é "none"
     food = request.GET.get('food', None)  # Pegando a comida que foi buscada, se nada foi buscado entao o valor é "none"
     restaurant = request.GET.get('restaurant_name', None)  # Pegando o restaurante que foi buscado, se nada foi buscado entao o valor é "none"
+    category = request.GET.get('category', None)  # Pegando a categoria selecionada, se nada foi selecionado entao o valor é "none"
     
     restaurantes = Restaurants.objects.all() # Pega todos os restaurantes salvos
     if restaurant: restaurantes = restaurantes.filter(name__icontains=restaurant) # Mantenho apenas os que são da cidade buscada em 'city'
     if city: restaurantes = restaurantes.filter(city__icontains=city) # Mantenho apenas os que são da cidade buscada em 'city'
     if food: restaurantes = restaurantes.filter(menu__name__icontains=food) # Mantenho apenas os que tem a comida buscada em 'food'
+    if category: restaurantes = restaurantes.filter(category__name__icontains=category) # Mantenho apenas os que tem a comida buscada em 'food'
 
     filtros = [
         {'param': 'pub', 'icon': 'local_bar', 'text': 'Pub'},
-        {'param': 'fogao_a_lenha', 'icon': 'local_fire_department', 'text': 'Fogão a lenha'},
-        {'param': 'cultura_local', 'icon': 'public', 'text': 'Cultura local'},
+        {'param': 'lenha', 'icon': 'local_fire_department', 'text': 'Fogão a lenha'},
+        {'param': 'local', 'icon': 'public', 'text': 'Cultura local'},
         {'param': 'piscina', 'icon': 'pool', 'text': 'Com piscina'},
         {'param': 'praia', 'icon': 'beach_access', 'text': 'Perto da praia'},
         {'param': 'gastronomia', 'icon': 'restaurant', 'text': 'Gastronomia de autor'},
-        {'param': 'romantico', 'icon': 'favorite', 'text': 'Ambiente romântico'},
-        {'param': 'vegetariano', 'icon': 'leaf', 'text': 'Opções vegetarianas'},
-        {'param': 'familia', 'icon': 'family_restroom', 'text': 'Para toda a família'},
-        {'param': 'musica_ao_vivo', 'icon': 'music_note', 'text': 'Música ao vivo'},
-        {'param': 'espetaculo', 'icon': 'theater_comedy', 'text': 'Espetáculo'}
+        {'param': 'romântico', 'icon': 'favorite', 'text': 'Ambiente romântico'},
+        {'param': 'vegetariana', 'icon': 'leaf', 'text': 'Opções vegetarianas'},
+        {'param': 'família', 'icon': 'family_restroom', 'text': 'Para toda a família'},
+        {'param': 'música', 'icon': 'music_note', 'text': 'Música ao vivo'},
+        {'param': 'espetáculo', 'icon': 'theater_comedy', 'text': 'Espetáculo'}
     ]
 
 
     # flg_teve_busca é para indicar se o usuário buscou algo, só para controlar a mensagem exibida na tela inicial
-    return render(request, 'index.html', {'restaurantes': restaurantes, 'flg_teve_busca': bool(city or food or restaurant), 'filtros': filtros})
+    return render(request, 'index.html', {'restaurantes': restaurantes, 'flg_teve_busca': bool(city or food or restaurant or category), 'filtros': filtros})
